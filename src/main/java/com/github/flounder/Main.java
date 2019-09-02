c847c094849e4b4c8c46dc735d93f9e809425427
zyw271828 <zyw_0618@outlook.com>
2019-09-02 20:43:30 +0800

    Update Main.fxml

--- src/main/java/com/github/flounder/Main.java
+++ src/main/java/com/github/flounder/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/Main.fxml"));
-            Scene scene = new Scene(root, 400, 400);
+            Scene scene = new Scene(root, 800, 270);
             scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
             primaryStage.setScene(scene);
+            primaryStage.setTitle("Flounder - an asymmetric encryption and decryption tool");
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
