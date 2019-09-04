0b34a040ec0034f75c32ac919142cccf9572096c
zyw271828 <zyw_0618@outlook.com>
2019-09-04 19:33:04 +0800

    Update MainController

--- src/main/java/com/github/flounder/MainController.java
+++ src/main/java/com/github/flounder/MainController.java
@@ -17,19 +17,64 @@ public class MainController {
     private JFXButton generateBtn;
 
     @FXML
+    private JFXButton encryptTextBtn;
+
+    @FXML
+    private JFXButton encryptFileBtn;
+
+    @FXML
+    private JFXButton decryptTextBtn;
+
+    @FXML
+    private JFXButton decryptFileBtn;
+
+    @FXML
+    private JFXButton exitBtn;
+
+    @FXML
     public void initialize() {
 
     }
 
     @FXML
     void onGenerateBtnClick(ActionEvent event) {
+        showStage("GenerateView", "Generate Keys", 600, 400);
+    }
+
+    @FXML
+    void onEncryptTextBtnClick(ActionEvent event) {
+        showStage("EncryptView", "Encrypt Text", 1024, 768);
+    }
+
+    @FXML
+    void onEncryptFileBtnClick(ActionEvent event) {
+        showStage("EncryptView", "Encrypt File", 1024, 768);
+    }
+
+    @FXML
+    void onDecryptTextBtnClick(ActionEvent event) {
+        showStage("DecryptView", "Decrypt Text", 1024, 768);
+    }
+
+    @FXML
+    void onDecryptFileBtnClick(ActionEvent event) {
+        showStage("DecryptView", "Decrypt File", 1024, 768);
+    }
+
+    @FXML
+    void onExitBtnClick(ActionEvent event) {
+        Stage stage = (Stage) exitBtn.getScene().getWindow();
+        stage.close();
+    }
+
+    private void showStage(String viewName, String stageTitle, double width, double height) {
         Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("/GenerateView.fxml"));
+            root = FXMLLoader.load(getClass().getResource("/" + viewName + ".fxml"));
             Stage stage = new Stage();
             stage.setAlwaysOnTop(true);
-            stage.setTitle("Generate Keys");
-            stage.setScene(new Scene(root, 600, 400));
+            stage.setTitle(stageTitle);
+            stage.setScene(new Scene(root, width, height));
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
