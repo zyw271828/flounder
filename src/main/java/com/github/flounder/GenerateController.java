dd5816978fea2883409b6bf5f137c2fca9bdf35a
zyw271828 <zyw_0618@outlook.com>
2019-09-04 09:09:41 +0800

    Add controllers and views

---
+++ src/main/java/com/github/flounder/GenerateController.java
@@ -0,0 +1,5 @@
+package com.github.flounder;
+
+public class GenerateController {
+
+}
--- src/main/java/com/github/flounder/MainController.java
+++ src/main/java/com/github/flounder/MainController.java
@@ -1,5 +1,38 @@
 package com.github.flounder;
 
+import java.io.IOException;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class MainController {
 
+    @FXML
+    private JFXButton generateBtn;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    void onGenerateBtnClick(ActionEvent event) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/GenerateView.fxml"));
+            Stage stage = new Stage();
+            stage.setAlwaysOnTop(true);
+            stage.setTitle("Generate Keys");
+            stage.setScene(new Scene(root, 600, 400));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
